8053f5da98b5bee81060f13bb513e4cfc5692032
liuyang <taoguangyanghui111@gmail.com>
2023-09-27 17:44:30 +0800

    ok111

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 let http = require('http');
-let users = [{ id: 1, name: 'zhufeng11' }, { id: 2, name: 'zhufeng2' }, { id: 3, name: 'zhufeng3' }];
+let users = [{ id: 1, name: 'zhufeng111' }, { id: 2, name: 'zhufeng2' }, { id: 3, name: 'zhufeng3' }];
 let server = http.createServer(function (req, res) {
   console.log(req.method, req.url);
   if (req.url == '/api/users') {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.end(JSON.stringify(users));
   } else {
-    res.end('Now Found!');
+    res.end('Now Found!!!');
   }
 });
 server.listen(3000, () => {
